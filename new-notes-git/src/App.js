70e4da36b5bb392b15e52b557768a04c6ffcb117
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-02-24 04:40:40 +0400

    react-notes-app

---
+++ new-notes-git/src/App.js
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from "react";
+import Header from "./components/Header";
+import NoteList from "./components/NoteList";
+import Search from "./components/Search";
+
+const App = () => {
+  const [notes, setNotes] = useState([
+    {
+      id: crypto.randomUUID(),
+      text: "This is my first note!",
+      date: "15/04/2021",
+    },
+    {
+      id: crypto.randomUUID(),
+      text: "This is my second note!",
+      date: "21/04/2021",
+    },
+    {
+      id: crypto.randomUUID(),
+      text: "This is my third note!",
+      date: "28/04/2021",
+    },
+    {
+      id: crypto.randomUUID(),
+      text: "This is my new note!",
+      date: "30/04/2021",
+    },
+  ]);
+
+  const [searchText, setSearchText] = useState("");
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const savedNotes = JSON.parse(
+      localStorage.getItem("react-notes-app-data")
+      );
+
+      if (savedNotes){
+        setNotes(savedNotes)
+      }
+  },
+
+   []);
+
+  useEffect(() => {
+    localStorage.setItem("react-notes-app-data", JSON.stringify(notes));
+  }, [notes]);
+
+  const addNote = (text) => {
+    const date = new Date();
+    const newNote = {
+      id: crypto.randomUUID(),
+      text: text,
+      date: date.toLocaleDateString(),
+    };
+    const newNotes = [...notes, newNote];
+    setNotes(newNotes);
+  };
+
+  const deleteNote = (id) => {
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+  };
+
+  return (
+    <div className={`${darkMode && "dark-mode"}`}>
+      <div className="container">
+        <Header handleToggleDarkMode={setDarkMode} />
+        <Search handleSearchNote={setSearchText} />
+        <NoteList
+          notes={notes.filter((note) =>
+            note.text.toLocaleLowerCase().includes(searchText)
+          )}
+          handleAddNote={addNote}
+          handleDeleteNote={deleteNote}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default App;
